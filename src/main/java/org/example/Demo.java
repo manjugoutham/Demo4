24a0460b6c917a164ed5622bef6e75a63fce7e21
Goutham-BL <goutham.y@bridgelabz.com>
2023-08-26 19:41:02 +0530

    Added : Demo branch file

--- src/main/java/org/example/Demo.java
+++ src/main/java/org/example/Demo.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub");
+        System.out.println("Hello Demo1 branch");
     }
 }
